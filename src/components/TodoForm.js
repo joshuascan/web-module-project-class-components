7fa459f5cb23f86f2cfb5aaa62c6fa114729ab6a
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-31 16:53:29 -0700

    WIP

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+class TodoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+}
+
+export default TodoForm;
