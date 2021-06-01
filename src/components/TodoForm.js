b997f0974aca4674533f23fbe60f145b04b9ec0b
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-31 17:28:39 -0700

    Create click handlers for TodoForm

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -8,12 +8,26 @@ class TodoForm extends React.Component {
     };
   }
 
+  handleChanges = (e) => {
+    this.setState({ input: e.target.value });
+  };
+
+  handleAdd = (e) => {
+    e.preventDefault();
+    this.props.addTask(this.state.input);
+  };
+
+  handleClear = (e) => {
+    e.preventDefault();
+    this.props.clearCompleted();
+  };
+
   render() {
     return (
       <form>
-        <input type="text" name="todo" />
-        <button>Add Task</button>
-        <button>Clear Completed</button>
+        <input onChange={this.handleChanges} type="text" name="todo" />
+        <button onClick={this.handleAdd}>Add Task</button>
+        <button onClick={this.handleClear}>Clear Completed</button>
       </form>
     );
   }
